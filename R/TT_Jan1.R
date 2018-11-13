1b90482bcdfc59ad63f0c6e084810209fad68268
emelaas <eli.melaas@gmail.com>
2018-11-13 14:21:47 -0500

    add TT_Jan1 model and pars

---
+++ R/TT_Jan1.R
@@ -0,0 +1,43 @@
+#' Thermal Time model as defined in
+#' Basler et al. 2016 (Agr. For. Meteorlogy)
+#'
+#' @param data input data (see reference for detailed description),
+#' data should be formatted using flat_format()
+#' @param par a vector of parameter values, this is functions specific
+#' @return raster or vector with estimated phenophase timing (in DOY)
+#' @keywords phenology, model
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' estimate = TT_Jan1(data = data, par = par)
+#'}
+
+TT_Jan1 = function(par, data ){
+
+  # exit the routine as some parameters are missing
+  if (length(par) != 2){
+    stop("model parameter(s) out of range (too many, too few)")
+  }
+
+  # extract the parameter values from the
+  # par argument for readability
+  T_base = par[1]
+  F_crit = par[2]
+
+  # simple degree day sum setup
+  Rf = data$Ti - T_base
+  Rf[Rf < 0] = 0
+  Rf[1:102,] = 0
+
+  # DOY of budburst criterium
+  doy = apply(Rf,2, function(xt){
+    doy = data$doy[which(cumsum(xt) >= F_crit)[1]]
+    doy[is.na(doy)] = 9999
+    return(doy)
+  })
+
+  # set export format, either a rasterLayer
+  # or a vector
+  shape_model_output(data = data, doy = doy)
+}
